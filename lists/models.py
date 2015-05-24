5564cc03c67959bb1cff921caf59046b9f516ef2
Jim Triphahn <jimmtriphahn@gmail.com>
2015-05-23 23:48:34 -0700

    Set up database in settings; Model for Item created and associated migrations

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+"""
+class Item: <-- this declaration 'implicitly' inherits from emph(object)
+class Item(object): <-- this declaration 'explicitly' inherits from emph(object)
+"""
+class Item(models.Model):
+	text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -30,3 +31,36 @@ class HomePageTest(TestCase):
 			{'new_item_text': 'A new list item'}
 		)
 		self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		"""
+		Below we are creating two Items, setting the text field in the model to some string,
+		and, finally, saving them.
+		"""
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'The second item'
+		second_item.save()
+
+		#returns QuerySet
+		saved_items = Item.objects.all()
+		"""
+		The count method hits the database and returns the number of records.
+		This is preferable if the QuerySet isn't cached (i.e stored in a variable)
+		because we dont need to iterate over all the objects anyways.
+		"""
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		"""
+		Need to grab text with .text because we assign the objects, not their variables
+		above.
+		"""
+		self.assertEqual(first_saved_item.text, first_item.text)
+		self.assertEqual(second_saved_item.text, second_item.text)
