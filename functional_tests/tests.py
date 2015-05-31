a8fd80910d28cb1b363de06af9f1fc047bcf3302
Jim Triphahn <jimmtriphahn@gmail.com>
2015-05-31 13:04:00 -0700

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	#the setUp() method is overridden from TestCase. It does nothing by itself
 	def setUp(self):
