697ee165c0defa04449033ba5ccdba14d0328bb1
Jim Triphahn <jimmtriphahn@gmail.com>
2016-03-31 22:33:51 -0700

    New FT for sharing, move session creation to base

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -53,17 +53,3 @@ class MyListsTest(FunctionalTest):
 			self.browser.find_elements_by_link_text('My lists'),
 			[]
 		)
-
-	def create_pre_authenticated_session(self, email):
-		if self.against_staging:
-			session_key = create_session_on_server(self.server_host, email)
-		else:
-			session_key = create_pre_authenticated_session(email)
-		## to set a cookie we need to first visit the domain.
-		## 404 pages load the quickest
-		self.browser.get(self.server_url + "/404_no_such_url/")
-		self.browser.add_cookie(dict(
-			name=settings.SESSION_COOKIE_NAME,
-			value=session_key,
-			path='/',
-		))
